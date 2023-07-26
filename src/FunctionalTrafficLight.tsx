c0899696dd1ade2e04b164442c43d5d4497dfb6e
luishsilva <luizhrick@gmail.com>
2023-07-26 08:43:30 -0700

    Add state variable to traffic light functional component

--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,14 +1,39 @@
+import { useState } from "react";
+
 export const FunctionalTrafficLight = () => {
+  
+  const [lightColor, setLightColor] = useState<string>('red');
+
+  const handleClick = () => {
+    switch (lightColor) {
+      case 'red':
+        setLightColor('green')
+        break;
+      case 'green':
+          setLightColor('yellow')
+          break;
+      default:
+        setLightColor('red')
+        break;
+    }
+  }
+
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
         {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={`circle ${lightColor === 'red' ? 'red' : 'black'}`}></div>
+        <div className={`circle ${lightColor === 'green' ? 'green' : 'black'}`}></div>
+        <div className={`circle ${lightColor === 'yellow' ? 'yellow' : 'black'}`}></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button 
+        className="next-state-button"
+        onClick={handleClick}
+      >
+        Next State
+      </button>
     </div>
   );
 };
